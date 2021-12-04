1ada0694213c582e455d438b083a58ff045cef3f
Khayal Farziyev <khayal.farziyev@pashabank.az>
2021-12-04 04:06:10 -0800

    lesson 6: warmup oop

--- src/main/java/az/iktlab/juniors/java_lessons/lesson_7/FindPeopleCountEx.java
+++ src/main/java/az/iktlab/juniors/java_lessons/lesson_7/FindPeopleCountEx.java
@@ -6,9 +6,22 @@ public class FindPeopleCountEx {
 
 
     public static void main(String[] args) {
-
         int[] people = fillRndArr();
-        // write your code here...
+        int[] ages = splitAges(people);
+        printAges(ages);
+    }
+
+    private static void printAges(int[] ages) {
+        for (int i = 0; i < ages.length; i++)
+            System.out.printf("%s => %s \n", i, ages[i]);
+    }
+
+    private static int[] splitAges(int[] people) {
+        int[] ages = new int[150];
+        for (int person : people) {
+            ages[person]++;
+        }
+        return ages;
     }
 
     private static int[] fillRndArr() {
